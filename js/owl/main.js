1b6263206519a6b3eff52d9bc9b86942c57ca8c0
ferreira-wilton <73347708+ferreira-wilton@users.noreply.github.com>
2020-11-09 14:56:19 -0300

    Add files via upload

---
+++ js/owl/main.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+})
+
+function redirecionar(){
+    window.window.open("https://www.netflix.com/br/title/70178217");
+}
+
+function redirecionarimdb(){
+    window.window.open("https://www.imdb.com/title/tt1856010/");
+}
+
+
